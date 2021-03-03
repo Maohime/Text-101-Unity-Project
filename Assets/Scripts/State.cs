aea6e854aa17d5b7ce8d89b131320ef947899e72
Maohime <maohime6@gmail.com>
2021-03-03 19:53:47 +0000

    Initial Commit

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [SerializeField] State[] nextStates;
+
+    [TextArea(10, 14)] [SerializeField] string storyText;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
